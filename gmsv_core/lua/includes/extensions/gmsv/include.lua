96c61a6108427a8ed8be027fdd8a75c6d2b106f0
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2024-08-10 03:24:48 -0400

    Add IncludeServer
    Removes extra if satement clutter

--- gmsv_core/lua/includes/extensions/gmsv/include.lua
+++ gmsv_core/lua/includes/extensions/gmsv/include.lua
@@ -1,15 +1,21 @@
 -- Don't localize because of loadtrack, not that it would help much anyways
 
-function IncludeShared(Path)
-	AddCSLuaFile(Path)
-
-	include(Path)
-end
-
 function IncludeClient(Path)
 	if SERVER then
 		AddCSLuaFile(Path)
 	elseif CLIENT then
 		include(Path)
 	end
+end
+
+function IncludeServer(Path)
+	if SERVER then
+		include(Path)
+	end
+end
+
+function IncludeShared(Path)
+	AddCSLuaFile(Path)
+
+	include(Path)
 end
