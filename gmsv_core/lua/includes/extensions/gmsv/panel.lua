41001918bc3890b8818a7e6190a65c8133ed5244
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2024-08-10 03:45:36 -0400

    Add Panel:SlowerThink
    Every second

--- gmsv_core/lua/includes/extensions/gmsv/panel.lua
+++ gmsv_core/lua/includes/extensions/gmsv/panel.lua
@@ -4,22 +4,37 @@ local pcall = pcall
 local vgui_GetAll = vgui.GetAll
 
 local PANEL = FindMetaTable("Panel")
-
 local PANEL_IsVisible = PANEL.IsVisible
 
-timer.Create("Panel_SlowThink", 1, 0, function()
+local Second = false
+
+timer.Create("Panel_SlowThink", 0.5, 0, function()
 	local Success, ErrorMessage
 	local Panels = vgui_GetAll()
 
 	for i = 1, #Panels do
 		local CurrentPanel = Panels[i]
 		if not PANEL_IsVisible(CurrentPanel) then continue end
-		if not isfunction(CurrentPanel.SlowThink) then continue end
 
-		Success, ErrorMessage = pcall(CurrentPanel.SlowThink, CurrentPanel)
+		-- SlowThink every half second
+		if isfunction(CurrentPanel.SlowThink) then
+			Success, ErrorMessage = pcall(CurrentPanel.SlowThink, CurrentPanel)
 
-		if not Success then
-			ErrorNoHaltWithStack(ErrorMessage)
+			if not Success then
+				ErrorNoHaltWithStack(ErrorMessage)
+			end
+		end
+
+		-- SlowerThink every half second
+		if Second and isfunction(CurrentPanel.SlowerThink) then
+			Success, ErrorMessage = pcall(CurrentPanel.SlowerThink, CurrentPanel)
+
+			if not Success then
+				ErrorNoHaltWithStack(ErrorMessage)
+			end
 		end
 	end
+
+	-- Tell the next iteration where we are
+	Second = not Second
 end)
